5ee35133c8031210b611f7781923a670c121465a
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-12-10 22:12:19 +0800

    实现Upvalue

---
+++ go/ch10/src/luago/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"go/ch10/src/luago/api"
+	"go/ch10/src/luago/state"
+	"os"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		data, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		ls := state.New()
+		ls.Register("print", print)
+		ls.Load(data, os.Args[1], "b")
+		ls.Call(0, 0)
+	}
+}
+
+func print(ls api.LuaState) int {
+	nArgs := ls.GetTop()
+	for i := 1; i <= nArgs; i++ {
+		if ls.IsBoolean(i) {
+			fmt.Printf("%t", ls.ToBoolean(i))
+		} else if ls.IsString(i) {
+			fmt.Printf("%q", ls.ToString(i))
+		} else {
+			fmt.Printf("%v", ls.TypeName(ls.Type(i)))
+		}
+		if i < nArgs {
+			fmt.Printf("\t")
+		}
+	}
+	fmt.Printf("\n")
+	return 0
+}
